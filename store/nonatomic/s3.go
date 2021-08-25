14f0c25ed77f4983d097fc960f06592e1f05e602
Jean-Baptiste Besselat <jean-baptiste.besselat@adotmob.com>
2021-08-25 14:24:04 +0200

    wip

--- store/nonatomic/s3.go
+++ store/nonatomic/s3.go
@@ -1,1 +1,11 @@
 package nonatomic
+
+import (
+    "context"
+    "fmt"
+    "log"
+
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/s3"
+)
